yext/yext-go

commit feaee483747a2efe7d7c31f348160b76453405e5

Add APIError Response and APIError types
These new concrete types for API errors will allow more detailed tracking of
error type statistics in ETL runs.

-r=mkilpatrick

client.go
@@ -134,10 +134,13 @@ func (c *Client) Do(req *http.Request, v interface{}) error {
 }
 
 func CheckResponseError(res *http.Response) error {
+	body, _ := ioutil.ReadAll(res.Body)
 	if res.StatusCode == 404 {
 		return ResourceNotFound
-	} else if res.StatusCode < 200 || res.StatusCode >= 400 {
-		body, _ := ioutil.ReadAll(res.Body)
+	} else if res.StatusCode >= 400 && res.StatusCode < 500 {
+		aeResp := UnmarhsalAPIError(res.StatusCode, body)
+		return aeResp
+	} else if res.StatusCode < 200 || res.StatusCode >= 500 {
 		return errors.New(fmt.Sprintf("%d: %v", res.StatusCode, string(body)))
 	}
 	return nil

api_error.go
@@ -1,0 +1,49 @@
+package yext
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type APIError struct {
+	Message string `json:"message"`
+	Code    int    `json:"errorCode"`
+}
+
+type APIErrorResponse struct {
+	Errors   []*APIError `json:"errors"`
+	HTTPCode int
+}
+
+func (e *APIError) Error() string {
+	return fmt.Sprintf("Recieved error code: '%d', messsage was: '%s'", e.Code, e.Message)
+}
+
+func (e *APIErrorResponse) Error() string {
+	var errStr string
+
+	for _, err := range e.Errors {
+		errStr += fmt.Sprintf("%s\n", err.Error())
+	}
+	return errStr
+}
+
+func UnmarhsalAPIError(httpCode int, respBody []byte) (e *APIErrorResponse) {
+	if len(respBody) == 0 {
+		return nil
+	}
+
+	var errResp = new(APIErrorResponse)
+	err := json.Unmarshal(respBody, errResp)
+	if err != nil {
+		ae := &APIError{
+			Code:    -1,
+			Message: fmt.Sprintf("Unknown API Error %v", string(respBody)),
+		}
+		errResp.Errors = append(errResp.Errors, ae)
+	}
+
+	errResp.HTTPCode = httpCode
+
+	return errResp
+}

location_service.go
@@ -1,7 +1,6 @@
 package yext
 
 import (
-	"errors"
 	"fmt"
 	"regexp"
 )
@@ -51,7 +50,14 @@ func (l *LocationService) Get(id string) (*Location, error) {
 func validateCustomFields(cfs map[string]interface{}) error {
 	for k, _ := range cfs {
 		if !customFieldKeyRegex.Match([]byte(k)) {
-			return errors.New(fmt.Sprintf("Custom Fields must be specified by their ID (found %s)", k))
+			return &APIErrorResponse{
+				Errors: []*APIError{
+					&APIError{
+						Code:    -2,
+						Message: fmt.Sprintf("Custom Fields must be specified by their ID (found %s)", k),
+					},
+				},
+			}
 		}
 	}
 	return nil

commit 341f422fd250e4d969ce05d864b2632f402df419

Fix bug introduced by feaee48
Bug with where the body was read would cause non-error responses to fail to
unmarshal.

-r=mkilpatrick

client.go
@@ -134,14 +134,15 @@ func (c *Client) Do(req *http.Request, v interface{}) error {
 }
 
 func CheckResponseError(res *http.Response) error {
-	body, _ := ioutil.ReadAll(res.Body)
 	if res.StatusCode == 404 {
 		return ResourceNotFound
-	} else if res.StatusCode >= 400 && res.StatusCode < 500 {
-		aeResp := UnmarhsalAPIError(res.StatusCode, body)
-		return aeResp
-	} else if res.StatusCode < 200 || res.StatusCode >= 500 {
-		return errors.New(fmt.Sprintf("%d: %v", res.StatusCode, string(body)))
+	} else if res.StatusCode < 200 || res.StatusCode >= 400 {
+		body, _ := ioutil.ReadAll(res.Body)
+		if res.StatusCode >= 400 && res.StatusCode < 500 {
+			return UnmarhsalAPIError(res.StatusCode, body)
+		} else {
+			return errors.New(fmt.Sprintf("%d: %v", res.StatusCode, string(body)))
+		}
 	}
 	return nil
 }