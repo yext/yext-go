672932150d69c86c9047e1be5725d6b8b968ae31
Administrator <mhupman@gmail.com>
2016-05-28 18:24:38 -0400

    gofmt

--- location_test.go
+++ location_test.go
@@ -6,35 +6,35 @@ import (
 )
 
 func jsonString(l *Location) (error, string) {
-  buf, err := json.Marshal(l)
-  if err != nil {
-    return err, ""
-  }
+	buf, err := json.Marshal(l)
+	if err != nil {
+		return err, ""
+	}
 
-  return nil, string(buf)
+	return nil, string(buf)
 }
 
 func TestJSONSerialization(t *testing.T) {
-  type test struct {
-    l *Location
-    want string
-  }
+	type test struct {
+		l    *Location
+		want string
+	}
 
-  tests := []test{
-    {&Location{}, `{}`},
+	tests := []test{
+		{&Location{}, `{}`},
 		{&Location{City: nil}, `{}`},
 		{&Location{City: String("")}, `{"city":""}`},
 		{&Location{Languages: nil}, `{}`},
 		{&Location{Languages: []string(nil)}, `{}`},
 		{&Location{Languages: []string{}}, `{"languages":[]}`},
 		{&Location{Languages: []string{"English"}}, `{"languages":["English"]}`},
-  }
+	}
 
-  for _, test := range tests {
-    if err, got := jsonString(test.l); err != nil {
-      t.Error("Unable to convert", test.l, "to JSON:", err)
-    } else if got != test.want {
-      t.Errorf("json.Marshal(%#v) = %s; expected %s", test.l, got, test.want)
-    }
-  }
+	for _, test := range tests {
+		if err, got := jsonString(test.l); err != nil {
+			t.Error("Unable to convert", test.l, "to JSON:", err)
+		} else if got != test.want {
+			t.Errorf("json.Marshal(%#v) = %s; expected %s", test.l, got, test.want)
+		}
+	}
 }
