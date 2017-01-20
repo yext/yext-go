78a819fa0d0332efc54e7d361a6d99f4b4c60e0c
Ben McGinnis <bmcginnis@yext.com>
2017-01-20 13:06:47 -0500

    Change LabelIds getter to return UnorderedStrings

--- location.go
+++ location.go
@@ -410,7 +410,7 @@ func (y Location) GetLanguages() (v []string) {
 	return v
 }
 
-func (y Location) GetLabelIds() (v []string) {
+func (y Location) GetLabelIds() (v UnorderedStrings) {
 	if y.LabelIds != nil {
 		v = *y.LabelIds
 	}
