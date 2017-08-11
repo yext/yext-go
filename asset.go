208a18b6e0d592a85ce027b8d8d3eaba7f530044
Ryan Helmlinger <rhelmlinger@yext.com>
2017-08-11 12:43:32 -0400

    add asset service (closing out work done by rhemlinger)

---
+++ asset.go
@@ -0,0 +1,44 @@
+package yext
+
+type AssetType string
+
+const (
+	AssetTypeText  AssetType = "AssetTypeText"
+	AssetTypePhoto AssetType = "AssetTypePhoto"
+	AssetTypeVideo AssetType = "AssetTypeVideo"
+)
+
+type LabelOperator string
+
+const (
+	LabelOperatorAnd LabelOperator = "AND"
+	LabelOperatorOr  LabelOperator = "OR"
+)
+
+type Asset struct {
+	Id              string       `json:"id"`
+	Name            string       `json:"name"`
+	Type            AssetType    `json:"type"`
+	ForLocations    ForLocations `json:"forLocations"`
+	Description     string       `json:"description"`
+	Labels          []string     `json:"labels"`
+	Contents        []Content    `json:"contents"`        // Type:Text
+	PhotoUrl        string       `json:"photoUrl"`        // Type:Photo
+	Details         string       `json:"details"`         // Type:Photo
+	ClickthroughUrl string       `json:"clickthroughUrl"` // Type:Photo
+	AlternateText   string       `json:"alternateText"`   // Type:Photo
+	VideoUrl        string       `json:"videoUrl"`        // Type:Video
+}
+
+type ForLocations struct {
+	MappingType   string        `json:"mappingType"`
+	FolderId      string        `json:"folderId"`
+	LocationIds   []string      `json:"locationIds"`
+	LabelIds      []string      `json:"labelIds"`
+	LabelOperator LabelOperator `json:"labelOperator"`
+}
+
+type Content struct {
+	Content string `json:"content"`
+	Locale  string `json:"locale"`
+}
---
+++ asset_service.go
@@ -0,0 +1,85 @@
+package yext
+
+import "fmt"
+
+const assetsPath = "assets"
+
+var AssetListMaxLimit = 1000
+
+type AssetService struct {
+	client *Client
+}
+
+type AssetListResponse struct {
+	Count  int      `json:"count"`
+	Assets []*Asset `json:"assets"`
+}
+
+func (a *AssetService) ListAll() ([]*Asset, error) {
+	var assets []*Asset
+	var lr listRetriever = func(opts *ListOptions) (int, int, error) {
+		alr, _, err := a.List(opts)
+		if err != nil {
+			return 0, 0, err
+		}
+		assets = append(assets, alr.Assets...)
+		return len(alr.Assets), alr.Count, err
+	}
+
+	if err := listHelper(lr, &ListOptions{Limit: AssetListMaxLimit}); err != nil {
+		return nil, err
+	} else {
+		return assets, nil
+	}
+}
+
+func (a *AssetService) List(opts *ListOptions) (*AssetListResponse, *Response, error) {
+	requrl, err := addListOptions(assetsPath, opts)
+	if err != nil {
+		return nil, nil, err
+	}
+	var v AssetListResponse
+	r, err := a.client.DoRequest("GET", requrl, &v)
+	if err != nil {
+		return nil, r, err
+	}
+
+	return &v, r, nil
+}
+
+func (a *AssetService) Create(asset *Asset) (*Response, error) {
+	r, err := a.client.DoRequestJSON("POST", fmt.Sprintf("%s", assetsPath), asset, nil)
+	if err != nil {
+		return r, err
+	}
+
+	return r, nil
+}
+
+func (a *AssetService) Get(assetId string) (*Asset, *Response, error) {
+	var v Asset
+	r, err := a.client.DoRequest("GET", fmt.Sprintf("%s/%s", assetsPath, assetId), &v)
+	if err != nil {
+		return nil, r, err
+	}
+
+	return &v, r, nil
+}
+
+func (a *AssetService) Update(assetId string, asset *Asset) (*Response, error) {
+	r, err := a.client.DoRequestJSON("PUT", fmt.Sprintf("%s/%s", assetsPath, assetId), asset, nil)
+	if err != nil {
+		return r, err
+	}
+
+	return r, nil
+}
+
+func (a *AssetService) Delete(assetId string) (*Response, error) {
+	r, err := a.client.DoRequest("DELETE", fmt.Sprintf("%s/%s", assetsPath, assetId), nil)
+	if err != nil {
+		return r, err
+	}
+
+	return r, nil
+}
