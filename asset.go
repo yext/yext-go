ba0b16e5b48646f813fbf7d1abe2bc3381c29ca2
Upal Saha <usaha@yext.com>
2018-10-03 18:21:06 -0400

    CFT Assets: modify asset data model, add asset type registry to asset service

--- asset.go
+++ asset.go
@@ -1,11 +1,21 @@
 package yext
 
-type AssetType string
+type MappingType string
 
 const (
-	AssetTypeText  AssetType = "AssetTypeText"
-	AssetTypePhoto AssetType = "AssetTypePhoto"
-	AssetTypeVideo AssetType = "AssetTypeVideo"
+	MappingTypeNone     MappingType = "NO_ENTITIES"
+	MappingTypeAll      MappingType = "ALL_ENTITIES"
+	MappingTypeFolder   MappingType = "FOLDER"
+	MappingTypeEntities MappingType = "ENTITIES"
+)
+
+type UsageType string
+
+const (
+	UsageTypeProfileFields  UsageType = "PROFILE_FIELDS"
+	UsageTypeReviewResponse UsageType = "REVIEW_RESPONSE"
+	UsageTypeSocialPosting  UsageType = "SOCIAL_POSTING"
+	UsageTypeAllFields      UsageType = "ALL_PROFILE_FIELDS"
 )
 
 type LabelOperator string
@@ -15,30 +25,91 @@ const (
 	LabelOperatorOr  LabelOperator = "OR"
 )
 
+type AssetType string
+
+const (
+	ASSETTYPE_TEXT         AssetType = "text"
+	ASSETTYPE_IMAGE        AssetType = "image"
+	ASSETTYPE_VIDEO        AssetType = "video"
+	ASSETTYPE_COMPLEXIMAGE AssetType = "complexImage"
+	ASSETTYPE_COMPLEXVIDEO AssetType = "complexVideo"
+)
+
+type AssetUsageType string
+
 type Asset struct {
-	Id              string       `json:"id"`
-	Name            string       `json:"name"`
-	Type            AssetType    `json:"type"`
-	ForLocations    ForLocations `json:"forLocations"`
-	Description     string       `json:"description"`
-	Labels          []string     `json:"labels"`
-	Contents        []Content    `json:"contents"`        // Type:Text
-	PhotoUrl        string       `json:"photoUrl"`        // Type:Photo
-	Details         string       `json:"details"`         // Type:Photo
-	ClickthroughUrl string       `json:"clickthroughUrl"` // Type:Photo
-	AlternateText   string       `json:"alternateText"`   // Type:Photo
-	VideoUrl        string       `json:"videoUrl"`        // Type:Video
+	Id          *string           `json:"id"`
+	Name        *string           `json:"name"`
+	Type        AssetType         `json:"type"`
+	Description *string           `json:"description,omitempty"`
+	ForEntities *ForEntities      `json:"forEntities,omitempty"`
+	Usage       *[]AssetUsage     `json:"usage,omitempty"`
+	Locale      *string           `json:"locale,omitempty"`
+	Labels      *UnorderedStrings `json:"labels,omitempty"`
+	Owner       *uint64           `json:"owner,omitempty"`
+	Value       interface{}       `json:"value,omitempty"`
 }
 
-type ForLocations struct {
-	MappingType   string        `json:"mappingType"`
-	FolderId      string        `json:"folderId"`
-	LocationIds   []string      `json:"locationIds"`
-	LabelIds      []string      `json:"labelIds"`
-	LabelOperator LabelOperator `json:"labelOperator"`
+type ForEntities struct {
+	MappingType   MappingType       `json:"mappingType"`
+	FolderId      *string           `json:"folderId,omitempty"`
+	EntityIds     *[]string         `json:"entityIds,omitempty"`
+	LabelIds      *UnorderedStrings `json:"labelIds,omitempty"`
+	LabelOperator *LabelOperator    `json:"labelOperator,omitempty"`
 }
 
-type Content struct {
-	Content string `json:"content"`
-	Locale  string `json:"locale"`
+type AssetUsage struct {
+	Type       UsageType `json:"type"`
+	FieldNames *[]string `json:"fieldNames,omitempty"`
+}
+
+type AssetValue interface {
+	GetAssetType() AssetType
+}
+
+type TextValue string
+
+func (t TextValue) GetAssetType() AssetType {
+	return ASSETTYPE_TEXT
+}
+
+type Image struct {
+	Url           string  `json:"url"`
+	AlternateText *string `json:"alternateText,omitempty"`
+}
+
+type ImageValue struct {
+	Image *Image `json:"image,omitempty"`
+}
+
+func (i ImageValue) GetAssetType() AssetType {
+	return ASSETTYPE_IMAGE
+}
+
+type ComplexImageValue struct {
+	Image           *Image `json:"image,omitempty"`
+	ClickthroughURL string `json:"clickthroughUrl,omitempty"`
+	Details         string `json:"details,omitempty"`
+}
+
+func (ci ComplexImageValue) GetAssetType() AssetType {
+	return ASSETTYPE_COMPLEXIMAGE
+}
+
+type VideoValue struct {
+	Video *Video `json:"video,omitempty"`
+}
+
+func (i VideoValue) GetAssetType() AssetType {
+	return ASSETTYPE_VIDEO
+}
+
+type ComplexVideoValue struct {
+	Video           *Video `json:"image,omitempty"`
+	ClickthroughURL string `json:"clickthroughUrl,omitempty"`
+	Details         string `json:"details,omitempty"`
+}
+
+func (ci ComplexVideoValue) GetAssetType() AssetType {
+	return ASSETTYPE_COMPLEXVIDEO
 }
--- asset_service.go
+++ asset_service.go
@@ -1,13 +1,17 @@
 package yext
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 const assetsPath = "assets"
 
 var AssetListMaxLimit = 1000
 
 type AssetService struct {
-	client *Client
+	client   *Client
+	registry Registry
 }
 
 type AssetListResponse struct {
@@ -15,11 +19,65 @@ type AssetListResponse struct {
 	Assets []*Asset `json:"assets"`
 }
 
+func (a *AssetService) RegisterDefaultAssetValues() {
+	a.registry = make(Registry)
+	// ASSETTYPE_TEXT doesn't need to register because it's just a string
+	a.RegisterAssetValue(ASSETTYPE_IMAGE, &ImageValue{})
+	a.RegisterAssetValue(ASSETTYPE_VIDEO, &VideoValue{})
+	a.RegisterAssetValue(ASSETTYPE_COMPLEXIMAGE, &ComplexImageValue{})
+	a.RegisterAssetValue(ASSETTYPE_COMPLEXVIDEO, &ComplexVideoValue{})
+}
+
+func (a *AssetService) RegisterAssetValue(t AssetType, assetValue interface{}) {
+	a.registry.Register(string(t), assetValue)
+}
+
+func (a *AssetService) CreateAssetValue(t AssetType) (interface{}, error) {
+	return a.registry.Create(string(t))
+}
+
+func (a *AssetService) toAssetsWithValues(assets []*Asset) error {
+	for _, asset := range assets {
+		if err := a.toAssetWithValue(asset); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (a *AssetService) toAssetWithValue(asset *Asset) error {
+	if asset.Type == ASSETTYPE_TEXT {
+		return nil
+	}
+	var assetValueValsByKey = asset.Value.(map[string]interface{})
+	assetValueObj, err := a.CreateAssetValue(asset.Type)
+	if err != nil {
+		return err
+	}
+
+	// Convert into struct of Asset Value Type
+	assetValueJSON, err := json.Marshal(assetValueValsByKey)
+	if err != nil {
+		return fmt.Errorf("Marshaling asset value to JSON: %s", err)
+	}
+
+	err = json.Unmarshal(assetValueJSON, &assetValueObj)
+	if err != nil {
+		return fmt.Errorf("Unmarshaling asset value to JSON: %s", err)
+	}
+	asset.Value = assetValueObj
+	return nil
+}
+
 func (a *AssetService) ListAll() ([]*Asset, error) {
 	var assets []*Asset
 	var lr listRetriever = func(opts *ListOptions) (int, int, error) {
 		alr, _, err := a.List(opts)
 		if err != nil {
+			return 0, 0, err
+		}
+
+		if err = a.toAssetsWithValues(alr.Assets); err != nil {
 			return 0, 0, err
 		}
 		assets = append(assets, alr.Assets...)
@@ -57,13 +115,17 @@ func (a *AssetService) Create(asset *Asset) (*Response, error) {
 }
 
 func (a *AssetService) Get(assetId string) (*Asset, *Response, error) {
-	var v Asset
+	var v *Asset
 	r, err := a.client.DoRequest("GET", fmt.Sprintf("%s/%s", assetsPath, assetId), &v)
 	if err != nil {
 		return nil, r, err
 	}
 
-	return &v, r, nil
+	if err := a.toAssetWithValue(v); err != nil {
+		return nil, r, err
+	}
+
+	return v, r, nil
 }
 
 func (a *AssetService) Update(assetId string, asset *Asset) (*Response, error) {
