2855ca2f414831106e7244b846f6b10920998a05
Upal Saha <usaha@yext.com>
2018-10-03 18:21:06 -0400

    CFT Assets: modify asset data model

--- asset.go
+++ asset.go
@@ -1,11 +1,21 @@
 package yext
 
-type AssetType string
-
 const (
-	AssetTypeText  AssetType = "AssetTypeText"
-	AssetTypePhoto AssetType = "AssetTypePhoto"
-	AssetTypeVideo AssetType = "AssetTypeVideo"
+	AssetTypeText         = "text"
+	AssetTypeImage        = "image"
+	AssetTypeVideo        = "video"
+	AssetTypeComplexImage = "complexImage"
+	AssetTypeComplexVideo = "complexVideo"
+
+	AssetMappingTypeNone     = "NO_ENTITIES"
+	AssetMappingTypeAll      = "ALL_ENTITIES"
+	AssetMappingTypeFolder   = "FOLDER"
+	AssetMappingTypeEntities = "ENTITIES"
+
+	AssetUsageTypeProfileFields  = "PROFILE_FIELDS"
+	AssetUsageTypeReviewResponse = "REVIEW_RESPONSE"
+	AssetUsageTypeSocialPosting  = "SOCIAL_POSTING"
+	AssetUsageTypeAllFields      = "ALL_PROFILE_FIELDS"
 )
 
 type LabelOperator string
@@ -15,30 +25,44 @@ const (
 	LabelOperatorOr  LabelOperator = "OR"
 )
 
+type AssetUsageType string
+
 type Asset struct {
-	Id              string       `json:"id"`
-	Name            string       `json:"name"`
-	Type            AssetType    `json:"type"`
-	ForLocations    ForLocations `json:"forLocations"`
-	Description     string       `json:"description"`
-	Labels          []string     `json:"labels"`
-	Contents        []Content    `json:"contents"`        // Type:Text
-	PhotoUrl        string       `json:"photoUrl"`        // Type:Photo
-	Details         string       `json:"details"`         // Type:Photo
-	ClickthroughUrl string       `json:"clickthroughUrl"` // Type:Photo
-	AlternateText   string       `json:"alternateText"`   // Type:Photo
-	VideoUrl        string       `json:"videoUrl"`        // Type:Video
+	Id          *string       `json:"id"`
+	Name        *string       `json:"name"`
+	Type        *string       `json:"type"`
+	Description *string       `json:"description,omitempty"`
+	ForEntities *ForEntities  `json:"forEntities,omitempty"`
+	Usage       *[]AssetUsage `json:"usage,omitempty"`
+	Locale      *string       `json:"locale,omitempty"`
+	Labels      *[]string     `json:"labels,omitempty"`
+	Owner       *uint64       `json:"owner,omitempty"`
+	Value       *AssetValue   `json:"value,omitempty"`
 }
 
-type ForLocations struct {
-	MappingType   string        `json:"mappingType"`
-	FolderId      string        `json:"folderId"`
-	LocationIds   []string      `json:"locationIds"`
-	LabelIds      []string      `json:"labelIds"`
-	LabelOperator LabelOperator `json:"labelOperator"`
+type ForEntities struct {
+	MappingType   *string        `json:"mappingType"`
+	FolderId      *string        `json:"folderId,omitempty"`
+	EntityIds     *[]string      `json:"entityIds,omitempty"`
+	LabelIds      *[]string      `json:"labelIds,omitempty"`
+	LabelOperator *LabelOperator `json:"labelOperator,omitempty"`
 }
 
-type Content struct {
-	Content string `json:"content"`
-	Locale  string `json:"locale"`
+type AssetValue struct {
+	Text  *TextValue  `json:"text,omitempty"`
+	Image *ImageValue `json:"image,omitempty"`
+}
+
+type AssetUsage struct {
+	Type       *string   `json:"type"`
+	FieldNames *[]string `json:"fieldNames"`
+}
+
+type TextValue string
+
+type ImageValue struct {
+	Url             *string `json:"url"`
+	AlternateText   *string `json:"alternateText,omitempty"`
+	ClickthroughURL *string `json:"clickthroughUrl,omitempty"`
+	Details         *string `json:"details,omitempty"`
 }
