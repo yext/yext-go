c3c30d59f73e8043fc60859a2e9642080ef20996
Upal Saha <usaha@yext.com>
2018-10-16 16:23:24 -0400

    Assets: Restore old asset service endpoints

--- asset.go
+++ asset.go
@@ -1,94 +1,35 @@
 package yext
 
-type MappingType string
-
 const (
-	MappingTypeNone     MappingType = "NO_ENTITIES"
-	MappingTypeAll      MappingType = "ALL_ENTITIES"
-	MappingTypeFolder   MappingType = "FOLDER"
-	MappingTypeEntities MappingType = "ENTITIES"
-)
-
-type UsageType string
-
-const (
-	UsageTypeProfileFields  UsageType = "PROFILE_FIELDS"
-	UsageTypeReviewResponse UsageType = "REVIEW_RESPONSE"
-	UsageTypeSocialPosting  UsageType = "SOCIAL_POSTING"
-	UsageTypeAllFields      UsageType = "ALL_PROFILE_FIELDS"
-)
-
-type LabelOperator string
-
-const (
-	LabelOperatorAnd LabelOperator = "AND"
-	LabelOperatorOr  LabelOperator = "OR"
-)
-
-type AssetType string
-
-const (
-	ASSETTYPE_TEXT         AssetType = "text"
-	ASSETTYPE_IMAGE        AssetType = "image"
-	ASSETTYPE_VIDEO        AssetType = "video"
-	ASSETTYPE_COMPLEXIMAGE AssetType = "complexImage"
-	ASSETTYPE_COMPLEXVIDEO AssetType = "complexVideo"
+	AssetTypeText  AssetType = "AssetTypeText"
+	AssetTypePhoto AssetType = "AssetTypePhoto"
+	AssetTypeVideo AssetType = "AssetTypeVideo"
 )
 
 type Asset struct {
-	Id          *string           `json:"id,omitempty"`
-	Name        *string           `json:"name,omitempty"`
-	Type        AssetType         `json:"type,omitempty"`
-	Description *string           `json:"description,omitempty"`
-	ForEntities *ForEntities      `json:"forEntities,omitempty"`
-	Usage       *[]AssetUsage     `json:"usage,omitempty"`
-	Locale      *string           `json:"locale,omitempty"`
-	Labels      *UnorderedStrings `json:"labels,omitempty"`
-	Owner       *uint64           `json:"owner,omitempty"`
-	Value       interface{}       `json:"value,omitempty"`
+	Id              string       `json:"id"`
+	Name            string       `json:"name"`
+	Type            AssetType    `json:"type"`
+	ForLocations    ForLocations `json:"forLocations"`
+	Description     string       `json:"description"`
+	Labels          []string     `json:"labels"`
+	Contents        []Content    `json:"contents"`        // Type:Text
+	PhotoUrl        string       `json:"photoUrl"`        // Type:Photo
+	Details         string       `json:"details"`         // Type:Photo
+	ClickthroughUrl string       `json:"clickthroughUrl"` // Type:Photo
+	AlternateText   string       `json:"alternateText"`   // Type:Photo
+	VideoUrl        string       `json:"videoUrl"`        // Type:Video
 }
 
-type ForEntities struct {
-	MappingType   MappingType       `json:"mappingType,omitempty"`
-	FolderId      *string           `json:"folderId,omitempty"`
-	EntityIds     *[]string         `json:"entityIds,omitempty"`
-	LabelIds      *UnorderedStrings `json:"labelIds,omitempty"`
-	LabelOperator LabelOperator     `json:"labelOperator,omitempty"`
+type ForLocations struct {
+	MappingType   string        `json:"mappingType"`
+	FolderId      string        `json:"folderId"`
+	LocationIds   []string      `json:"locationIds"`
+	LabelIds      []string      `json:"labelIds"`
+	LabelOperator LabelOperator `json:"labelOperator"`
 }
 
-type AssetUsage struct {
-	Type       UsageType `json:"type,omitempty"`
-	FieldNames *[]string `json:"fieldNames,omitempty"`
-}
-
-type TextValue string
-
-type ImageValue struct {
-	Url           string `json:"url,omitempty"`
-	AlternateText string `json:"alternateText,omitempty"`
-	Height        uint64 `json:height,omitempty`
-	Width         uint64 `json:width,omitempty`
-}
-
-type ComplexImageValue struct {
-	Image           *ImageValue `json:"image,omitempty"`
-	Description     string      `json:"description,omitempty"`
-	Details         string      `json:"details,omitempty"`
-	ClickthroughURL string      `json:"clickthroughUrl,omitempty"`
-}
-
-type VideoValue struct {
-	Url string `json:"url,omitempty"`
-}
-
-type ComplexVideoValue struct {
-	Video       *VideoValue `json:"video,omitempty"`
-	Description string      `json:"description,omitempty"`
-}
-
-func (a *Asset) GetId() string {
-	if a.Id == nil {
-		return ""
-	}
-	return *a.Id
+type Content struct {
+	Content string `json:"content"`
+	Locale  string `json:"locale"`
 }
---
+++ cftasset_service.go
@@ -0,0 +1,148 @@
+package yext
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+const assetsPath = "assets"
+
+var AssetListMaxLimit = 1000
+
+type CFTAssetService struct {
+	client   *Client
+	registry Registry
+}
+
+type CFTAssetListResponse struct {
+	Count  int         `json:"count"`
+	Assets []*CFTAsset `json:"assets"`
+}
+
+func (a *CFTAssetService) RegisterDefaultAssetValues() {
+	a.registry = make(Registry)
+	// ASSETTYPE_TEXT doesn't need to register because it's just a string
+	a.RegisterAssetValue(ASSETTYPE_IMAGE, &ImageValue{})
+	a.RegisterAssetValue(ASSETTYPE_VIDEO, &VideoValue{})
+	a.RegisterAssetValue(ASSETTYPE_COMPLEXIMAGE, &ComplexImageValue{})
+	a.RegisterAssetValue(ASSETTYPE_COMPLEXVIDEO, &ComplexVideoValue{})
+}
+
+func (a *CFTAssetService) RegisterAssetValue(t AssetType, assetValue interface{}) {
+	a.registry.Register(string(t), assetValue)
+}
+
+func (a *CFTAssetService) CreateAssetValue(t AssetType) (interface{}, error) {
+	return a.registry.Create(string(t))
+}
+
+func (a *CFTAssetService) toAssetsWithValues(assets []*CFTAsset) error {
+	for _, asset := range assets {
+		if err := a.toAssetWithValue(asset); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (a *CFTAssetService) toAssetWithValue(asset *CFTAsset) error {
+	if asset.Type == ASSETTYPE_TEXT {
+		asset.Value = asset.Value.(string)
+		return nil
+	}
+	var assetValueValsByKey = asset.Value.(map[string]interface{})
+	assetValueObj, err := a.CreateAssetValue(asset.Type)
+	if err != nil {
+		return err
+	}
+
+	// Convert into struct of Asset Value Type
+	assetValueJSON, err := json.Marshal(assetValueValsByKey)
+	if err != nil {
+		return fmt.Errorf("Marshaling asset value to JSON: %s", err)
+	}
+
+	err = json.Unmarshal(assetValueJSON, &assetValueObj)
+	if err != nil {
+		return fmt.Errorf("Unmarshaling asset value to JSON: %s", err)
+	}
+	asset.Value = assetValueObj
+	return nil
+}
+
+func (a *CFTAssetService) ListAll() ([]*CFTAsset, error) {
+	var assets []*CFTAsset
+	var lr listRetriever = func(opts *ListOptions) (int, int, error) {
+		alr, _, err := a.List(opts)
+		if err != nil {
+			return 0, 0, err
+		}
+
+		if err = a.toAssetsWithValues(alr.Assets); err != nil {
+			return 0, 0, err
+		}
+		assets = append(assets, alr.Assets...)
+		return len(alr.Assets), alr.Count, err
+	}
+
+	if err := listHelper(lr, &ListOptions{Limit: AssetListMaxLimit}); err != nil {
+		return nil, err
+	} else {
+		return assets, nil
+	}
+}
+
+func (a *CFTAssetService) List(opts *ListOptions) (*CFTAssetListResponse, *Response, error) {
+	requrl, err := addListOptions(assetsPath, opts)
+	if err != nil {
+		return nil, nil, err
+	}
+	var v CFTAssetListResponse
+	r, err := a.client.DoRequest("GET", requrl, &v)
+	if err != nil {
+		return nil, r, err
+	}
+
+	return &v, r, nil
+}
+
+func (a *CFTAssetService) Create(asset *CFTAsset) (*Response, error) {
+	r, err := a.client.DoRequestJSON("POST", fmt.Sprintf("%s", assetsPath), asset, nil)
+	if err != nil {
+		return r, err
+	}
+
+	return r, nil
+}
+
+func (a *CFTAssetService) Get(assetId string) (*CFTAsset, *Response, error) {
+	var v *CFTAsset
+	r, err := a.client.DoRequest("GET", fmt.Sprintf("%s/%s", assetsPath, assetId), &v)
+	if err != nil {
+		return nil, r, err
+	}
+
+	if err := a.toAssetWithValue(v); err != nil {
+		return nil, r, err
+	}
+
+	return v, r, nil
+}
+
+func (a *CFTAssetService) Update(assetId string, asset *CFTAsset) (*Response, error) {
+	r, err := a.client.DoRequestJSON("PUT", fmt.Sprintf("%s/%s", assetsPath, assetId), asset, nil)
+	if err != nil {
+		return r, err
+	}
+
+	return r, nil
+}
+
+func (a *CFTAssetService) Delete(assetId string) (*Response, error) {
+	r, err := a.client.DoRequest("DELETE", fmt.Sprintf("%s/%s", assetsPath, assetId), nil)
+	if err != nil {
+		return r, err
+	}
+
+	return r, nil
+}
