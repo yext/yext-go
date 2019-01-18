9ff2ec237e3a0fd1caae502e0a8e9c83ca3d2440
Byron Harvey <bharvey@yext.com>
2019-01-18 15:35:48 -0500

    Hard-encode all requests to the yext api for # characters

--- client.go
+++ client.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -97,6 +98,7 @@ func (c *Client) NewRootRequestJSON(method string, path string, obj interface{})
 }
 
 func (c *Client) NewRequestBody(method string, fullPath string, data []byte) (*http.Request, error) {
+	fullPath = strings.Replace(fullPath, "#", "%23", -1)
 	req, err := http.NewRequest(method, fullPath, bytes.NewBuffer(data))
 	if err != nil {
 		return nil, err
