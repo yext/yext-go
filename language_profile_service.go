3f00a534550b9c82a90988a2c9fc94a6fa08b8f3
Ben Haines <bhainesva@users.noreply.github.com>
2019-03-18 17:35:33 -0400

    add support for LanguageProfile:ListAll (#107)
    * add support for LanguageProfile:ListAll

--- language_profile_service.go
+++ language_profile_service.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 )
 
-const entityProfilesPath = "entityprofiles"
+const (
+	entityProfilesPath          = "entityprofiles"
+	LanguageProfileListMaxLimit = 50
+)
 
 type LanguageProfileService struct {
 	client   *Client
@@ -13,6 +16,15 @@ type LanguageProfileService struct {
 
 type LanguageProfileListResponse struct {
 	Profiles []interface{} `json:"profiles"`
+}
+
+type LanguageProfileListAllResponse struct {
+	Count        int    `json:"count"`
+	PageToken    string `json:"pageToken"`
+	ProfileLists []struct {
+		Profiles []interface{} `json:"profiles"`
+	} `json:"profileLists"`
+	typedProfiles []Entity
 }
 
 func (l *LanguageProfileService) RegisterDefaultEntities() {
@@ -39,10 +51,10 @@ func (l *LanguageProfileService) Get(id string, languageCode string) (*Entity, *
 	return &entity, r, nil
 }
 
-func (l *LanguageProfileService) List(id string) ([]*Entity, *Response, error) {
+func (l *LanguageProfileService) List(id string) ([]Entity, *Response, error) {
 	var (
 		v        LanguageProfileListResponse
-		profiles = []*Entity{}
+		profiles = []Entity{}
 	)
 	r, err := l.client.DoRequest("GET", fmt.Sprintf("%s/%s", entityProfilesPath, id), &v)
 	if err != nil {
@@ -55,9 +67,80 @@ func (l *LanguageProfileService) List(id string) ([]*Entity, *Response, error) {
 	}
 	for _, profile := range typedProfiles {
 		setNilIsEmpty(profile)
-		profiles = append(profiles, &profile)
+		profiles = append(profiles, profile)
 	}
 	return profiles, r, nil
+}
+
+func (l *LanguageProfileService) ListAll(opts *EntityListOptions) ([]Entity, error) {
+	var entities []Entity
+	if opts == nil {
+		opts = &EntityListOptions{}
+	}
+	opts.ListOptions = ListOptions{Limit: EntityListMaxLimit}
+	var lg tokenListRetriever = func(listOptions *ListOptions) (string, error) {
+		opts.ListOptions = *listOptions
+		resp, _, err := l.listAllHelper(opts)
+		if err != nil {
+			return "", err
+		}
+
+		for _, entity := range resp.typedProfiles {
+			entities = append(entities, entity)
+		}
+		return resp.PageToken, nil
+	}
+
+	if err := tokenListHelper(lg, &opts.ListOptions); err != nil {
+		return nil, err
+	}
+	return entities, nil
+}
+
+func (l *LanguageProfileService) listAllHelper(opts *EntityListOptions) (*LanguageProfileListAllResponse, *Response, error) {
+	var (
+		requrl = entityProfilesPath
+		err    error
+	)
+
+	if opts != nil {
+		requrl, err = addEntityListOptions(requrl, opts)
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+
+	if opts != nil {
+		requrl, err = addListOptions(requrl, &opts.ListOptions)
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+
+	v := &LanguageProfileListAllResponse{}
+	r, err := l.client.DoRequest("GET", requrl, v)
+	if err != nil {
+		return nil, r, err
+	}
+
+	entityInterfaces := []interface{}{}
+	for _, list := range v.ProfileLists {
+		for _, entity := range list.Profiles {
+			entityInterfaces = append(entityInterfaces, entity)
+		}
+	}
+
+	typedEntities, err := l.registry.ToEntityTypes(entityInterfaces)
+	if err != nil {
+		return nil, r, err
+	}
+
+	for _, entity := range typedEntities {
+		setNilIsEmpty(entity)
+	}
+
+	v.typedProfiles = typedEntities
+	return v, r, nil
 }
 
 func (l *LanguageProfileService) Upsert(entity Entity, id string, languageCode string) (*Response, error) {
