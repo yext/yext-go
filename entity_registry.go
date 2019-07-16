eb84b3fe41354f93be50749672260615a38965b1
czou <czou@yext.com>
2019-07-16 12:09:32 -0400

    Add and register Hotel entity

--- entity_registry.go
+++ entity_registry.go
@@ -15,6 +15,7 @@ func defaultEntityRegistry() *EntityRegistry {
 	registry.Register(string(ENTITYTYPE_HEALTHCAREPROFESSIONAL), &HealthcareProfessionalEntity{})
 	registry.Register(string(ENTITYTYPE_HEALTHCAREFACILITY), &HealthcareFacilityEntity{})
 	registry.Register(string(ENTITYTYPE_ATM), &ATMEntity{})
+	registry.Register(string(ENTITYTYPE_HOTEL), &HotelEntity{})
 	entityRegistry := EntityRegistry(registry)
 	return &entityRegistry
 }
