a49bbd45b65309fe7a7bd45b8c4c9ca2783c5313
= <jdavid@yext.com>
2020-02-07 15:29:57 -0500

    add job entity

---
+++ entity_job.go
@@ -0,0 +1,92 @@
+package yext
+
+import (
+	"encoding/json"
+)
+
+const ENTITYTYPE_JOB EntityType = "job"
+
+type JobEntity struct {
+	BaseEntity
+
+	//Job Info
+	Name           *string   `json:"name,omitempty"`
+	Description    *string   `json:"description,omitempty"`
+	Timezone       *string   `json:"timezone,omitempty"`
+	EmploymentType *string   `json:"employmentType,omitempty"`
+	DatePosted     *string   `json:"datePosted,omitempty"`
+	ValidThrough   *string   `json:"validThrough,omitempty"`
+	Keywords       *[]string `json:"keywords,omitempty"`
+
+	// Urls
+	ApplicationURL *string `json:"applicationURL,omitempty"`
+	LandingPageURL *string `json:"landingPageUrl,omitempty"`
+}
+
+func (j JobEntity) GetId() string {
+	if j.BaseEntity.Meta != nil && j.BaseEntity.Meta.Id != nil {
+		return *j.BaseEntity.Meta.Id
+	}
+	return ""
+}
+
+func (j JobEntity) GetName() string {
+	if j.Name != nil {
+		return *j.Name
+	}
+	return ""
+}
+
+func (j JobEntity) GetDescription() string {
+	if j.Description != nil {
+		return *j.Description
+	}
+	return ""
+}
+
+func (j JobEntity) GetTimezone() string {
+	if j.Timezone != nil {
+		return *j.Timezone
+	}
+	return ""
+}
+
+func (j JobEntity) GetEmploymentType() string {
+	if j.EmploymentType != nil {
+		return *j.EmploymentType
+	}
+	return ""
+}
+
+func (j JobEntity) GetDatePosted() string {
+	if j.DatePosted != nil {
+		return *j.DatePosted
+	}
+	return ""
+}
+
+func (j JobEntity) GetExpiryDate() string {
+	if j.ValidThrough != nil {
+		return *j.ValidThrough
+	}
+	return ""
+}
+
+func (j JobEntity) GetApplicationURL() string {
+	if j.ApplicationURL != nil {
+		return *j.ApplicationURL
+	}
+	return ""
+}
+
+func (j JobEntity) GetLandingPageUrl() string {
+	if j.LandingPageURL != nil {
+		return *j.LandingPageURL
+	}
+	return ""
+}
+
+func (j *JobEntity) String() string {
+	b, _ := json.Marshal(j)
+	return string(b)
+}
