5198a92d1b2dae99e0a045c65cf6b19208c8dde0
afitzgerald <afitzgerald@yext.com>
2021-05-06 15:59:45 -0400

    adds accessHours profile field to locations

--- entity_location.go
+++ entity_location.go
@@ -50,6 +50,7 @@ type LocationEntity struct {
 
 	// Location Info
 	Hours                     **Hours           `json:"hours,omitempty"`
+	AccessHours               **Hours           `json:"accessHours,omitempty"`
 	DriveThroughHours         **Hours           `json:"driveThroughHours,omitempty"`
 	Closed                    **bool            `json:"closed,omitempty"`
 	Description               *string           `json:"description,omitempty"`
