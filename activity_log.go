9af3deb46f471214cabc8c63a9db83692341c247
zxu <zxu@yext.com>
2021-06-17 13:08:47 -0400

    Add Activity Logs Struct and Service
    J=PC-134868

---
+++ activity_log.go
@@ -0,0 +1,52 @@
+package yext
+
+type ActivityLog struct {
+	TimeStamp  *int    `json:"timestamp"`
+	LocationID *string `json:"locationId"`
+	Details    *string `json:"details"`
+	Content    *string `json:"content"`
+	Type       *string `json:"type"`
+	Actor      *string `json:"actor"`
+}
+
+func (y ActivityLog) GetTimeStamp() int {
+	if y.TimeStamp != nil {
+		return *y.TimeStamp
+	}
+	return 0
+}
+
+func (y ActivityLog) GetLocationID() string {
+	if y.LocationID != nil {
+		return *y.LocationID
+	}
+	return ""
+}
+
+func (y ActivityLog) GetDetails() string {
+	if y.Details != nil {
+		return *y.Details
+	}
+	return ""
+}
+
+func (y ActivityLog) GetContent() string {
+	if y.Content != nil {
+		return *y.Content
+	}
+	return ""
+}
+
+func (y ActivityLog) GetType() string {
+	if y.Type != nil {
+		return *y.Type
+	}
+	return ""
+}
+
+func (y ActivityLog) GetActor() string {
+	if y.Actor != nil {
+		return *y.Actor
+	}
+	return ""
+}
---
+++ activity_log_service.go
@@ -0,0 +1,49 @@
+package yext
+
+const (
+	activityLogPath = "analytics/activity"
+
+	// Use the following constants for Activity Filter
+	ACTIVITYTYPES_LOCATION_UPDATED              = "LOCATION_UPDATED"
+	ACTIVITYTYPES_PUBLISHER_SUGGESTION_CREATED  = "PUBLISHER_SUGGESTION_CREATED"
+	ACTIVITYTYPES_PUBLISHER_SUGGESTION_APPROVED = "PUBLISHER_SUGGESTION_APPROVED"
+	ACTIVITYTYPES_PUBLISHER_SUGGESTION_REJECTED = "PUBLISHER_SUGGESTION_REJECTED"
+	ACTIVITYTYPES_REVIEW_CREATED                = "REVIEW_CREATED"
+	ACTIVITYTYPES_SOCIAL_POST_CREATED           = "SOCIAL_POST_CREATED"
+	ACTIVITYTYPES_LISTING_LIVE                  = "LISTING_LIVE"
+	ACTIVITYTYPES_DUPLICATE_SUPPRESSED          = "DUPLICATE_SUPPRESSED"
+
+	ACTORS_YEXT_SYSTEM       = "YEXT_SYSTEM"
+	ACTORS_SCHEDULED_CONTENT = "SCHEDULED_CONTENT"
+	ACTORS_API               = "API"
+	ACTORS_PUBLISHER         = "PUBLISHER"
+)
+
+type ActivityLogService struct {
+	client *Client
+}
+
+type ActivityLogResponse struct {
+	Data []*ActivityLog `json:"activities"`
+}
+
+type ActivityFilter struct {
+	StartDate     *string   `json:"startDate"`
+	EndDate       *string   `json:"endDate"`
+	LocationIds   *[]string `json:"locationIds"`
+	ActivityTypes *[]string `json:"activityTypes"` // See const above
+	Actors        *[]string `json:"actors"`        // See const above
+}
+
+type ActivityLogRequest struct {
+	Limit   int             `json:"limit"`
+	Offset  int             `json:"offset"`
+	Filters *ActivityFilter `json:"filters"`
+}
+
+// ActivityLogRequest can be nil, no filter
+func (a *ActivityLogService) Create(req *ActivityLogRequest) (*ActivityLogResponse, *Response, error) {
+	arr := &ActivityLogResponse{}
+	r, err := a.client.DoRequestJSON("POST", activityLogPath, req, arr)
+	return arr, r, err
+}
