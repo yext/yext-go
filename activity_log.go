ee8629b6d3fa20c9a88a393168b935063cf282c2
zxu <zxu@yext.com>
2021-06-25 13:41:35 -0400

    update actordetails to activity log

--- activity_log.go
+++ activity_log.go
@@ -1,12 +1,18 @@
 package yext
 
 type ActivityLog struct {
-	TimeStamp  *int    `json:"timestamp"`
-	LocationID *string `json:"locationId"`
-	Details    *string `json:"details"`
-	Content    *string `json:"content"`
-	Type       *string `json:"type"`
-	Actor      *string `json:"actor"`
+	TimeStamp    *int        `json:"timestamp"`
+	LocationID   *string     `json:"locationId"`
+	Details      *string     `json:"details"`
+	Content      *string     `json:"content"`
+	Type         *string     `json:"type"`
+	Actor        *string     `json:"actor"`
+	ActorDetails ActorDetail `json:"actorDetails"` // need to set version 20210728
+}
+
+type ActorDetail struct {
+	Name  *string `json:"name"`
+	Email *string `json:"emails"`
 }
 
 func (y ActivityLog) GetTimeStamp() int {
