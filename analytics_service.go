d7497b67474db73632ded6df427d4e35dd390e1b
Mohammad Usman <musman@yext.com>
2023-02-22 12:02:53 +0000

    add support for listing impressions metric

--- analytics_service.go
+++ analytics_service.go
@@ -10,6 +10,7 @@ type AnalyticsFilters struct {
 	StartDate                        *string   `json:"startDate"`
 	EndDate                          *string   `json:"endDate"`
 	LocationIds                      *[]string `json:"locationIds"`
+	EntityIds                        *[]string `json:"entityIds"`
 	EntityTypes                      *[]string `json:"entityType"`
 	FolderId                         *int      `json:"folderId"`
 	Countries                        *[]string `json:"countries"`
